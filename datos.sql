e3a5b5bb40d97c074e96ac7993bdcad9a5317d7a
diana <d1234@outlook.es>
2025-05-15 10:08:59 -0500

    tercer comentario

---
+++ datos.sql
@@ -0,0 +1,12 @@
+use  BDPROYECTO;
+insert into tipo_de_documento (Nomdoc) values ("CC"),("TI");
+insert into tipo_de_documento (Nomdoc) values ("PASAPORTE"),("CE"),("RC");
+insert into método_de_pago (Nombre) values ("TC"),("TD"),("BV"), ("TRANSFERENCIA");
+
+insert into usuario 
+values (123,"diana","leon","dd@outlook.com","34567",1);
+insert into usuario values (345, "ferney","salamanca","","485844",1);
+insert into usuario values (8965, "pepito","perez","","54637",2);
+insert into usuario values (6342, "juanita","lopez","","7865342",3);
+
+
